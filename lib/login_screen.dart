799a82bfe7e253ce12be77019aebc723fcfdd0f9
manikandanlevontech <manikandan.levontechno@gmail.com>
2025-05-09 11:24:09 +0530

    Created new branch and change small letters to Caps lettes on SIGN IN add again push in main branch

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -15,7 +15,7 @@ class LoginScreen extends StatelessWidget {
             children: [
               Image.asset('assets/images/signin_balls.png'),
               const Text(
-                'Sign in.',
+                'SIGN IN.',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
               ),
               const SizedBox(
